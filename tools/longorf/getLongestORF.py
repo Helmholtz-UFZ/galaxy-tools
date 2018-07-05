8cb82daad7560b036c9a955a9ed7b9417c2ee240
hagto <eribor@gmx.de>
2018-07-05 17:13:10 +0200

    Due to changing to getfastabed for getting fasta sequences, the fasta headers look different and the script was accordingly adapted.

--- tools/longorf/getLongestORF.py
+++ tools/longorf/getLongestORF.py
@@ -2,29 +2,13 @@
 #input: a amino acid fasta file of all open reading frames (ORF) listed by transcript (output of GalaxyTool "getorf")
 
 #example:
-
-#>253936-254394(+)_1 [28 - 63] 
-#LTNYCQMVHNIL
-#>253936-254394(+)_2 [18 - 77] 
-#HKLIDKLLPNGAQYFVKSTQ
-#>253936-254394(+)_3 [32 - 148] 
-#QTTAKWCTIFCKKYPVAPFHTMYLNYAVTWHHRSLLVAV
-#>253936-254394(+)_4 [117 - 152] 
-#LGIIVPSLLLCN
-#>248351-252461(+)_1 [14 - 85] 
-#VLARKYPRCLSPSKKSPCQLRQRS
-#>248351-252461(+)_2 [21 - 161] 
-#PGNTHDASAHRKSLRVNSDKEVKCLFTKNAASEHPDHKRRRVSEHVP
-#>248351-252461(+)_3 [89 - 202] 
-#VPLHQECCIGAPRPQTTACVRACAMTNTPRSSMTSKTG
-#>248351-252461(+)_4 [206 - 259] 
-#SRTTSGRQSVLSEKLWRR
-#>248351-252461(+)_5 [263 - 313] 
-#CLSPLWVPCCSRHSCHG
+#>STRG.1.1(-)_1 [10 - 69]
+#GGNHHTLGGKKTFSYTHPPC
+#>STRG.1.1(-)_2 [3 - 80]
+#FLRGEPPHIGGKKDIFLHPPTLLKGR
 
 #output1: fasta file with all longest ORFs per transcript
-#output2: table with information about seqID, start, end, length, orientation, longest for all ORFs
-
+#output2: table with information about seqID, start, end, strand, length, sense, longest? for all ORFs
 
 import sys,re;
 
@@ -39,19 +23,17 @@ def findlongestOrf(transcriptDict,old_seqID):
 			i_max = i;
 
 	for i in range(0,len(prevTranscript)):
-
-		prevStart = prevTranscript[i][0];
-		prevEnd = prevTranscript[i][1];
-		prevLength = prevTranscript[i][2];
-
-		output = str(old_seqID) + "\t" + str(prevStart) + "\t" + str(prevEnd) + "\t" + str(prevLength);
+		prevORFstart = prevTranscript[i][0];
+		prevORFend = prevTranscript[i][1];
+		prevORFlength = prevTranscript[i][2];
+		header = prevTranscript[i][3];
+		strand = re.search('\(([+-]+)\)',header).group(1);
 		
-		if (end - start > 0):
-			output+="\tForward";
+		output = str(header) + "\t" + str(prevORFstart) + "\t" + str(prevORFend) + "\t" + str(prevORFlength) + "\t" + str(strand);
+		if (prevORFend - prevORFstart > 0):
+			output+="\tnormal";
 		else:
-			output+="\tReverse";
-
-
+			output+="\treverse_sense";
 		if(i == i_max):
 			output += "\ty\n";
 		else:
@@ -62,6 +44,8 @@ def findlongestOrf(transcriptDict,old_seqID):
 	transcriptDict.pop(old_seqID, None);
 	return None;
 
+#-----------------------------------------------------------------------------------------------------
+
 INPUT = open(sys.argv[1],"r");
 OUTPUT_FASTA = open(sys.argv[2],"w");
 OUTPUT_ORF_SUMMARY = open(sys.argv[3],"w");
@@ -70,24 +54,21 @@ seqID = "";
 old_seqID = "";
 lengthDict = {};
 seqDict = {};
-headerDict = {}
+headerDict = {};
 transcriptDict = {};
 
 skip = False;
 
-OUTPUT_ORF_SUMMARY.write("seqID\tstart\tend\tlength\torientation\tlongest\n");
+OUTPUT_ORF_SUMMARY.write("seqID\torf_start\torf_end\tlength\tstrand\tsense\tlongest\n");
 
 for line in INPUT:
 	line = line.strip();
-#	print line;
 	if(re.match(">",line)): #header
-
+		header = line
 		seqID = "_".join(line.split(">")[1].split("_")[:-1])
-		#seqID = line.split(">")[1].split("_")[0];
-		start = int (re.search('\ \[(\d+)\ -', line).group(1));
-		end = int (re.search('-\ (\d+)\]',line).group(1));
-		length = abs(end - start);
-			
+		ORFstart = int (re.search('\ \[(\d+)\ -', line).group(1));
+		ORFend = int (re.search('-\ (\d+)\]',line).group(1));
+		length = abs(ORFend - ORFstart);
 
 		if(seqID not in transcriptDict and old_seqID != ""): #new transcript
 			findlongestOrf(transcriptDict,old_seqID);
@@ -95,33 +76,24 @@ for line in INPUT:
 		if seqID not in transcriptDict:
 			transcriptDict[seqID] = [];
 
-		transcriptDict[seqID].append([start,end,length]);
-
-
+		transcriptDict[seqID].append([ORFstart,ORFend,length,header]);
 
 		if(seqID not in lengthDict and old_seqID != ""): #new transcript
-
 			#write FASTA
 			OUTPUT_FASTA.write(headerDict[old_seqID]+"\n"+seqDict[old_seqID]+"\n");
-
-
 			#delete old dict entry
 			headerDict.pop(old_seqID, None);
 			seqDict.pop(old_seqID, None);
 			lengthDict.pop(old_seqID, None);
-
 		#if several longest sequences exist with the same length, the dictionary saves the last occuring.
 		if(seqID not in lengthDict or length >= lengthDict[seqID]):
 			headerDict[seqID] = line;
 			lengthDict[seqID] = length;
 			seqDict[seqID] = "";
 			skip = False;
-
 		else:
 			skip = True;
 			next;
-
-
 		old_seqID = seqID;
 	elif(skip):
 		next;

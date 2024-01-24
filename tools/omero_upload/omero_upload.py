06ec1fccbc89169a5a5b92b41e17af6869a4f380
Riccardo Massei <riccardo.massei@ufz.de>
2024-01-24 15:52:02 +0100

    initial commit

--- tools/omero_upload/omero_upload.py
+++ tools/omero_upload/omero_upload.py
@@ -1,55 +1,43 @@
 import argparse
-import omero
-from omero.rtypes import rdouble, rint, rstring
-import ezomero
 import os
-import pandas as pd
-from omero.gateway import BlitzGateway
+import time
 from datetime import datetime
 
-now = datetime.now() # current date and time
-date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+import ezomero
+import omero
+from omero.rtypes import rstring
+from omero.gateway import BlitzGateway
 
-parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
 
-parser.add_argument("--url", required=True, type=str, help="")
-parser.add_argument("--port", required=True, type=int, help="")
-parser.add_argument("--username", required=True, type=str, help="")
-parser.add_argument("--password", required=True, type=str, help="")
-parser.add_argument("--folder", required=True, type=str, help="")
+def import_omero_biotox(user, passw, pth, host='omero.intranet.ufz.de', port=4064):
+    start_time = time.time()
+    image_transferred = 0
+    conn = BlitzGateway(user, passw, host=host, port=port, secure=True)
+    if conn.connect() is False:
+        print("Not connected to OMERO instance. Please retry")
+    else:
+        print("Connection established. Starting Importing.")
+        conn.SERVICE_OPTS.setOmeroGroup('-1')
+        dataset_obj = omero.model.DatasetI()
+        dataset_obj.setName(rstring(datetime.now()))
+        dataset_obj = conn.getUpdateService().saveAndReturnObject(dataset_obj, conn.SERVICE_OPTS)
+        dataset_id = dataset_obj.getId().getValue()
+        list_img = os.listdir(pth)
+        for image_name in list_img:
+            image_path = os.path.join(pth, image_name)
+            ezomero.ezimport(conn, image_path, dataset=dataset_id)
+            image_transferred += 1
+        print(f"Total {image_transferred} in dataset {dataset_id}")
+        print("--- Execution time %s seconds ---" % (time.time() - start_time))
+        conn.close()
 
+
+parser = argparse.ArgumentParser(prog='omero_upload', description='')
+parser.add_argument('--url', type=str, default="omero.intranet.ufz.de", help='URL')
+parser.add_argument('--port', type=int, default=4064, help='port')
+parser.add_argument('--username', type=str, help='username')
+parser.add_argument('--password', type=str, help='password')
+parser.add_argument('--folder', type=str, help='folder')
 args = parser.parse_args()
 
-print(args.url)
-
-conn = BlitzGateway(arg.username , arg.password , host= arg.url, port= arg.port, secure= True)
-constatus = str(conn.connect())
-
-tb_input = knio.input_tables[0].to_pandas()
-list = tb_input.values.tolist()
-dict = dict(list)
-
-# create a dataset to import into the dataset
-dataset_obj = omero.model.DatasetI()
-dataset_obj.setName(rstring(date_time))
-dataset_obj = conn.getUpdateService().saveAndReturnObject(dataset_obj, conn.SERVICE_OPTS)
-dataset_id = dataset_obj.getId().getValue()
-dataset_id_str = str(dataset_id)
-
-list_img = os.listdir(path)
-
-for image_name in list:
-    image_path = os.path.join(path, image_name)
-    ezomero.ezimport(conn, image_path, dataset=dataset_id, ann=dict, ns="VAST Metadata")
-    image_transferred += 1
-
-image_transferred = str(image_transferred)
-df = pd.DataFrame({
-    'Output': ['Connecton Status', 'Image Import', 'Dataset Name'. 'Numbe of Images Transferred"],
-    'constatus': [constatus, "Import Completed", dataset_id_str, image_transferred],
-})
-
-conn.close()
+import_omero_biotox(args.user, args.password, args.folder, args.url, args.port)

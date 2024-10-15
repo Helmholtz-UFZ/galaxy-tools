a0511de53d5122a6da7878f9f168bb341326d1b3
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2024-10-15 11:27:03 +0000

    Updated all the scripts and xml files according to previous review

--- tools/omero/omero_filter.py
+++ tools/omero/omero_filter.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import json
+import sys
 
 import ezomero as ez
 
@@ -12,37 +13,38 @@ def filter_ids_ezo(user, pws, host, port, filter, id, value1, value2=None, tsv_f
     id = list(map(int, id))
 
     # Function to write tabular file from the ezomero output
-    def write_ids_to_tsv(data, header):
-        with open(tsv_file, 'a+', newline='') as f:
-            f.seek(0)
-            is_empty = f.tell() == 0  # Check if file is empty
+    def write_ids_to_tsv(data):
+        with open(tsv_file, 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
-            if is_empty:
-                writer.writerow([header])  # Write the header
             for item in data:
                 writer.writerow([item])  # Write each ID
 
-    try:
-        with ez.connect(user, pws, "", host, port, secure=True) as conn:
+try:
+    with ez.connect(user, pws, "", host, port, secure=True) as conn:
+        try:
             if filter == "filename":
                 fn_ids = ez.filter_by_filename(conn, id, value1)
-                write_ids_to_tsv(fn_ids, [f"Images with filename {value1}"])
+                write_ids_to_tsv(fn_ids)
                 return fn_ids
+
             elif filter == "KP":
                 kp_ims = ez.filter_by_kv(conn, id, value1, value2)
-                write_ids_to_tsv(kp_ims, [f"Images with KV {value1} and {value2}"])
+                write_ids_to_tsv(kp_ims)
                 return kp_ims
+
             elif filter == "tag":
                 tg_dict = ez.filter_by_tag_value(conn, id, value1)
-                write_ids_to_tsv(tg_dict, [f"Images with tag {value1}"])
+                write_ids_to_tsv(tg_dict)
                 return tg_dict
 
             else:
-                raise ValueError(f"Unsupported object type")
+                raise ValueError(f"Unsupported object type: {filter}")
 
-    except Exception as e:
-        print(f"Connection error: {str(e)}")
-        return None
+        except ValueError as ve:
+            sys.exit(f"ValueError: {str(ve)}")
+
+except Exception as e:
+    sys.exit(f"Connection error: {str(e)}")
 
 
 # Argument parsing
@@ -62,10 +64,12 @@ if __name__ == "__main__":
                         help="First searching values - Filename, Key, Tag")
     parser.add_argument('--value2', required=False,
                         help="Second searching values - Value (necessary just for Key-Value Pairs filter")
-    parser.add_argument('--tsv_file', default='filter_list.tsv', required=True,
+    parser.add_argument('--tsv_file', default='filter_list.tsv', 
                         help="Output TSV file path.")
-
     args = parser.parse_args()
+
+    if args.filter == "KP" and args.value2 is None:
+        raise ValueError("'--value 2' is necessary to retrieve KP")
 
     with open(args.credential_file, 'r') as f:
         crds = json.load(f)
--- tools/omero/omero_get_id.py
+++ tools/omero/omero_get_id.py
@@ -6,29 +6,29 @@ import ezomero as ez
 
 
 def get_ids_ezo(user, pws, host, port, final_obj_type, parent_obj_type, parent_id=None, tsv_file="id_list.tsv"):
+
     # Function to write tabular file from the ezomero output
-    def write_ids_to_tsv(data, header):
-        with open(tsv_file, 'a+', newline='') as f:
-            f.seek(0)
-            is_empty = f.tell() == 0  # Check if file is empty
+    def write_ids_to_tsv(data):
+        with open(tsv_file, 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
-            if is_empty:
-                writer.writerow([header])  # Write the header
             for item in data:
                 writer.writerow([item])  # Write each ID
-    try:
-        with ez.connect(user, pws, "", host, port, secure=True) as conn:
+try:
+    with ez.connect(user, pws, "", host, port, secure=True) as conn:
+        try:
             if final_obj_type == "Project":
                 proj_ids = ez.get_project_ids(conn)
-                write_ids_to_tsv(proj_ids, "Project IDs")
+                write_ids_to_tsv(proj_ids)
                 return proj_ids
+
             elif final_obj_type == "Dataset":
                 args = {'project': None}
                 if parent_obj_type == "Project":
                     args['project'] = parent_id
                 ds_ids = ez.get_dataset_ids(conn, **args)
-                write_ids_to_tsv(ds_ids, "Dataset IDs")
+                write_ids_to_tsv(ds_ids)
                 return ds_ids
+
             elif final_obj_type == "Image":
                 args = {
                     'project': None,
@@ -46,31 +46,39 @@ def get_ids_ezo(user, pws, host, port, final_obj_type, parent_obj_type, parent_i
                     args['well'] = parent_id
                 elif parent_obj_type != "All":
                     raise ValueError("Object set as parent_obj_type is not compatible")
+                
                 ds_ims = ez.get_image_ids(conn, **args)
-                write_ids_to_tsv(ds_ims, "Image IDs")
+                write_ids_to_tsv(ds_ims)
                 return ds_ims
+
             elif final_obj_type == "Annotation":
                 map_annot_ids = ez.get_map_annotation_ids(conn, parent_obj_type, parent_id)
-                write_ids_to_tsv(map_annot_ids, "Annotation IDs")
+                write_ids_to_tsv(map_annot_ids)
                 return map_annot_ids
+
             elif final_obj_type == "Tag":
                 tag_ids = ez.get_tag_ids(conn, parent_obj_type, parent_id)
-                write_ids_to_tsv(tag_ids, "Tag IDs")
+                write_ids_to_tsv(tag_ids)
                 return tag_ids
+
             elif final_obj_type == "Roi":
                 roi_ids = ez.get_roi_ids(conn, parent_id)
-                write_ids_to_tsv(roi_ids, "ROI IDs")
+                write_ids_to_tsv(roi_ids)
                 return roi_ids
+
             elif final_obj_type == "Table":
                 file_ann_ids = ez.get_file_annotation_ids(conn, parent_obj_type, parent_id)
-                write_ids_to_tsv(file_ann_ids, "Table IDs")
+                write_ids_to_tsv(file_ann_ids)
                 return file_ann_ids
+
             else:
                 raise ValueError(f"Unsupported object type: {final_obj_type}")
 
-    except Exception as e:
-        print(f"Connection error: {str(e)}")
-        return None
+        except ValueError as ve:
+            sys.exit(f"ValueError: {str(ve)}")
+
+except Exception as e:
+    sys.exit(f"Connection error: {str(e)}")
 
 
 # Argument parsing
@@ -88,7 +96,7 @@ if __name__ == "__main__":
                         help="Type of object from which you fetch IDs: Project, Dataset, Plate, Well, Image (or 'All' if you want to get all objects).")
     parser.add_argument('--parent_id', required=False, type=int,
                         help="ID of the OMERO object in `--parent_obj_type`, not required if you used `--parent_obj_type All`.")
-    parser.add_argument('--tsv_file', default='id_list.tsv', required=True,
+    parser.add_argument('--tsv_file', default='id_list.tsv',
                         help="Output TSV file path.")
     args = parser.parse_args()
 

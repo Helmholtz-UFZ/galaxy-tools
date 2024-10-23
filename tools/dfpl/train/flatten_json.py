e3cf9f15eac992cfde7a75ea9f1bcdd9dc9d5f8c
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2024-10-23 12:20:02 +0200

    re-structured autoencoder parameters

--- tools/dfpl/train/flatten_json.py
+++ tools/dfpl/train/flatten_json.py
@@ -18,9 +18,10 @@ def flatten(d: dict):
 
 d = flatten(d)
 
-d["outputDir"] = "./output/"
 d["py/object"] = "dfpl.options.Options"
-
+d["outputDir"] = "./output/"
 d["ecModelDir"] = "autoencoder"  # The directory where the full encoder will be saved
+del d["use_autoencoder"]
+d["trainAC"] = bool(d["trainAC"] == "true")  # because this is actually a <select> tag that provides string values
 
 print(json.dumps(d))

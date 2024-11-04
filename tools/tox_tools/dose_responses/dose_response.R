8b68231ecc51d55546895662cd9e33769e55689d
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2024-11-04 09:31:34 +0100

    Tool enhancement - Tox Tools DR modelling  (#67)
    * add new tools (omero get and omero filter) and updated the ci/pr pipeline to add metadat to the dummy set)
    
    * use conditionals to more explicitely indicate the id required.
    
    * fix annotation part
    
    * convert omero_get to omero_get_id
    
    * add get_value
    
    * add single quotes
    
    * Fix bad copy paste
    
    * add tests
    
    * remove unused f
    
    * remove unused function
    
    * fix function name
    
    * fix spaces
    
    * Updated all the scripts and xml files according to previous review
    
    * correct indentation of omero_filter and omero_get_id
    
    * Update omero_filter.py
    
    Fixed the error handling using sys
    
    * fixed code and xml from omero_filter for correct linting
    
    * correct image name
    
    * fixed indentation
    
    * fix the regex of omero_get_value and updated the sheed
    
    * fixed the regex to handle single number plus comma separated numbers
    
    * Fix testing in output and omero_get_value.xml
    
    * Correct the cheetah sintax in omero_get_value.xml
    
    * Correct the test  in omero_get_value.xml - tsv output has 2 rows considering the header
    
    * Last corrections to the omero_get_value script to harmonize it with the other code style (writing tsv in write mode and error handling)
    
    * add macros
    
    * omero import do not expose password
    
    * add requirement macros
    
    * add host port token
    
    * bump
    
    * add macros file
    
    * fix macro expansion
    
    * Added security disclaimers to the OMERO suite tools
    
    * Added macro for security disclaimer
    
    * Updated the tox tools - Now compounds name in the graph and in the excel output
    
    * add some validators and fix the plot output name
    
    * Fix the regex
    
    * fix the test to input according to regex<3
    <2
    
    * fix the regex
    
    * fix the regex, part2
    
    * fix the regex, part 3
    
    ---------
    
    Co-authored-by: Lucille Delisle <lucille.delisle@epfl.ch>
    Co-authored-by: Matthias Bernt <m.bernt@ufz.de>

--- tools/tox_tools/dose_responses/dose_response.R
+++ tools/tox_tools/dose_responses/dose_response.R
@@ -27,7 +27,7 @@ calculate_ec_values <- function(model) {
     return(list(EC50 = ec50, EC25 = ec25, EC10 = ec10))
 }
 
-plot_dose_response <- function(model, data, ec_values, concentration_col, response_col, plot_file) {
+plot_dose_response <- function(model, data, ec_values, concentration_col, response_col, plot_file, compound_name, concentration_unit) {
     concentration_grid <- seq(min(data[[concentration_col]]), max(data[[concentration_col]]), length.out = 100)
     prediction_data <- data.frame(concentration_grid)
     colnames(prediction_data) <- concentration_col
@@ -37,8 +37,8 @@ plot_dose_response <- function(model, data, ec_values, concentration_col, respon
         geom_point(color = "red") +
         geom_line(data = prediction_data, aes_string(x = concentration_col, y = "response"), color = "blue") +
         geom_vline(xintercept = ec_values$EC10[1], color = "green", linetype = "dashed") +
-        geom_vline(xintercept = ec_values$EC50[1], color = "purple", linetype = "dashed") +
-        labs(title = "Dose-Response Curve", x = "Concentration", y = "Effect") +
+        geom_vline(xintercept = ec_values$EC50[1], color = "red", linetype = "dashed") +
+        labs(title = compound_name, " - Dose-Response Curve", x = "Dose [", concentration_unit, "]", y = "Response %") +
         theme_minimal() +
         theme(
             panel.background = element_rect(fill = "white", color = NA),
@@ -50,13 +50,13 @@ plot_dose_response <- function(model, data, ec_values, concentration_col, respon
     dev.off()
 }
 
-dose_response_analysis <- function(data, concentration_col, response_col, plot_file, ec_file) {
+dose_response_analysis <- function(data, concentration_col, response_col, plot_file, ec_file, compound_name, concentration_unit) {
     concentration_col <- colnames(data)[as.integer(concentration_col)]
     response_col <- colnames(data)[as.integer(response_col)]
     models <- fit_models(data, concentration_col, response_col)
     best_model_info <- select_best_model(models)
     ec_values <- calculate_ec_values(best_model_info$model)
-    plot_dose_response(best_model_info$model, data, ec_values, concentration_col, response_col, plot_file)
+    plot_dose_response(best_model_info$model, data, ec_values, concentration_col, response_col, plot_file, compound_name, concentration_unit)
 
     ec_data <- data.frame(
         EC10 = ec_values$EC10[1],
@@ -75,6 +75,8 @@ concentration_col <- args[2]
 response_col <- args[3]
 plot_file <- args[4]
 ec_file <- args[5]
+compound_name <- args[6]
+concentration_unit <- args[7]
 
 data <- read.csv(data_file, header = TRUE, sep = "\t")
-dose_response_analysis(data, concentration_col, response_col, plot_file, ec_file)
+dose_response_analysis(data, concentration_col, response_col, plot_file, ec_file, compound_name, concentration_unit)

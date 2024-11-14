06a0f7e00c7c06ccfee1ccb83ad6463066713589
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2024-11-14 14:48:54 +0100

    output h5 files

--- tools/dfpl/json_train.py
+++ tools/dfpl/json_train.py
@@ -4,9 +4,8 @@ from sys import stdin
 d = json.load(stdin)
 
 d["py/object"] = "dfpl.options.Options"
-d["outputDir"] = "./output/"
-d["ecModelDir"] = "autoencoder"  # The directory where the full encoder will be saved
-del d["use_autoencoder"]
+d["outputDir"] = "./model/"
+d["ecModelDir"] = "./autoencoder/"
 d["trainAC"] = bool(d["trainAC"] == "true")  # because this is actually a <select> tag that provides string values
 
 print(json.dumps(d))

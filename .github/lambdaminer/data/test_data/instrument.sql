8d21394347ba08bba52a5ad086016497a73c8de1
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2025-02-17 08:46:39 +0100

    Update instrument.sql

--- .github/lambdaminer/data/test_data/instrument.sql
+++ .github/lambdaminer/data/test_data/instrument.sql
@@ -6,8 +6,8 @@
 -- Data for Name: instrument; Type: TABLE DATA; Schema: public; Owner: lmdb_adm
 --
 
-INSERT INTO public.instrument (instrument_id, name, serial_number, location, institute, contact_mail, type, model, manufacturer, parameters) VALUES (1, 'solariX-12T-2XR', '1272500-00128', 'Leipzig', 'Department of Analytical Chemistry', 'oliver.lechtenfeld@ufz.de', 'ICR', 'solariX', 'Bruker', '{ "field_strength": "12T", "icr_cell": "2XR"}');
-INSERT INTO public.instrument (instrument_id, name, serial_number, location, institute, contact_mail, type, model, manufacturer, parameters) VALUES (2, 'scimaX-7T-2XR', '1859310-00244', 'Leipzig', 'Department of Analytical Chemistry', 'oliver.lechtenfeld@ufz.de', 'ICR', 'scimaX', 'Bruker', '{ "field_strength": "7T", "icr_cell": "2XR"}');
+INSERT INTO public.instrument (instrument_id, name, serial_number, location, institute, contact_mail, type, model, manufacturer, parameters) VALUES (1, 'solariX-12T-2XR', '1234567-12345', 'Halle', 'Department of Chemistry', 'max.muesterman@gmail.de', 'ICR', 'solariX', 'Bruker', '{ "field_strength": "12T", "icr_cell": "2XR"}');
+INSERT INTO public.instrument (instrument_id, name, serial_number, location, institute, contact_mail, type, model, manufacturer, parameters) VALUES (2, 'scimaX-7T-2XR', '1234567-12345', 'Halle', 'Department of Chemistry', 'max.muesterman@gmail.de', 'ICR', 'scimaX', 'Bruker', '{ "field_strength": "7T", "icr_cell": "2XR"}');
 
 --
 -- Name: instrument_instrument_id_seq; Type: SEQUENCE SET; Schema: public; Owner: lmdb_adm

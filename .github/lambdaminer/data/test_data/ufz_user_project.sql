24034681dc7d6f2e669ce0c9b8a09924eda1bd1e
wurz <johann.wurz@ufz.de>
2025-02-26 13:57:03 +0100

    add planemo as user and to projects

--- .github/lambdaminer/data/test_data/ufz_user_project.sql
+++ .github/lambdaminer/data/test_data/ufz_user_project.sql
@@ -1,4 +1,4 @@
--- SPDX-FileCopyrightText: 2024 Helmholtz Centre for Environmental Research GmbH - UFZ
+-- SPDX-FileCopyrightText: 2025 Helmholtz Centre for Environmental Research GmbH - UFZ
 --
 -- SPDX-License-Identifier: LicenseRef-UFZ-GPL-3.0-or-later
 
@@ -9,3 +9,5 @@
 INSERT INTO public.ufz_user_project (project_id, user_id) VALUES (1, 2);
 INSERT INTO public.ufz_user_project (project_id, user_id) VALUES (2, 2);
 INSERT INTO public.ufz_user_project (project_id, user_id) VALUES (2, 3);
+INSERT INTO public.ufz_user_project (project_id, user_id) VALUES (1, 7);
+INSERT INTO public.ufz_user_project (project_id, user_id) VALUES (2, 7);

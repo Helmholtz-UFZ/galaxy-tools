46cb63861d9db1c902a20320c3b65146e5c27d03
Thorben <stehling.t@web.de>
2025-02-28 15:00:23 +0100

    most important parameters work

--- tools/dfpl/json_predict_gnn.py
+++ tools/dfpl/json_predict_gnn.py
@@ -3,16 +3,19 @@ from sys import stdin
 
 d = json.load(stdin)
 
-d["no_cache"] = bool(d["no_cache"] == True)
-#wird dann data_path noch benötigt????
-d["test_path"] = "test-data/smiles.csv" 
+#d["no_cache"] = bool(d["no_cache"] == True)
+#d["no_cuda"] = bool(d["no_cuda"] == True)
+
+#d["num_workers"] = 1 #sys.env.get("GALAXY_SLOTS", "1")  
+
+#hier hardgecoded oder input dateien frei wählbar
+#d["test_path"] = "smiles.csv" 
 
 #Path to CSV file where predictions will be saved.
-d["preds_path"] = "test-data/DMPNN_preds.csv" 
+d["preds_path"] = "DMPNN_preds.csv" 
 
 #Directory from which to load model checkpoints "(walks directory and ensembles all models that are found)" -> führt das zu problem? besser einfach weg lassen?
 
-d["checkpoint_path"] = "test-data/dmpnn-random/fold_0/model_0/model.pt"
 
 
 d["py/object"] = "dfpl.options.GnnOptions"

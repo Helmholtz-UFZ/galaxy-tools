95d686d282052447796ecf35f7157ef6ca7e76bc
Matthias Bernt <m.bernt@ufz.de>
2025-03-27 17:32:35 +0100

    test db at /tmp

--- tools/dfast/test-data.sh
+++ tools/dfast/test-data.sh
@@ -4,7 +4,5 @@ set -ex
 
 wget https://dfast.annotation.jp/dfast_core_db.tar.gz
 tar -xvf dfast_core_db.tar.gz
-mv db/ tools/dfast/test-data/
-ls tools/dfast/test-data/
-ls tools/dfast/test-data/db/
+mv db/ /tmp/
 rm dfast_core_db.tar.gz

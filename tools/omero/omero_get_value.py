6f47dff3775455a8c45d39a3863a2306cd942443
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2025-04-08 16:57:25 +0200

    Tool enanchment: OMERO_get_value (#111)
    * Updated the omero_get_value to include attachments
    
    * Few fixes to pass the test locally
    
    * bump version
    
    * Python script clean-up
    
    Co-authored-by: M Bernt <m.bernt@ufz.de>
    
    * Update on the attachment fetching plus modified omero ci
    
    * update the PR
    
    * update the PR
    
    * correct the IDs value for the test
    
    * update the PR and CI
    
    * update the test
    
    * update the PR and CI
    
    * test of the python script
    
    * fix the xml
    
    * fix python
    
    * fix python
    
    * fix the xml
    
    * fix the xml
    
    * fix indentation
    
    * Correct the CI PR to select the correct FileAnnotation and match with the OriginalFile
    
    * further python script fix, save tsv location and linting
    
    * Apply suggestions from code review
    
    Co-authored-by: M Bernt <m.bernt@ufz.de>
    
    * last fix of the python script, save just the tabular files in the output path
    
    * remove tsv file after fetching
    
    * fix the test output
    
    * fix the test and file extension
    
    * fix the test to assert text content
    
    * elements name fixed
    
    * fix test
    
    * updated the PR and CI to add tables and KV to the dummy dataset
    
    * correct the yml file with KV pairs
    
    * correct the yml file with KV pairs
    
    * correct the omero metadata populate
    
    * correct the omero metadata populate also in the PR.yaml
    
    * updated the test, added Table and Annotations
    
    * Update dummy-bulkmap.csv
    
    * correct the xml file
    
    * Fix test
    
    * Fix test
    
    * Fix indentation
    
    * Update tools/omero/omero_get_value.xml
    
    * Update omero_get_value.py
    
    ---------
    
    Co-authored-by: M Bernt <m.bernt@ufz.de>

--- tools/omero/omero_get_value.py
+++ tools/omero/omero_get_value.py
@@ -1,15 +1,17 @@
 import argparse
 import csv
 import json
+import os
 import sys
 
 import ezomero as ez
+import pandas as pd
 
 
-def get_object_ezo(user, pws, host, port, obj_type, ids, tsv_file):
+def get_object_ezo(user, pws, host, port, obj_type, ids, out_dir):
     # Function to write tabular file from the ezomero output
     def write_values_to_tsv(data, header):
-        with open(tsv_file, 'w', newline='') as f:
+        with open("output.tsv", 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
             writer.writerow([header])  # Write the header
             for item in data:
@@ -17,15 +19,15 @@ def get_object_ezo(user, pws, host, port, obj_type, ids, tsv_file):
 
     # Function to write tabular file from a dictionary ezomero output
     def write_dict_to_tsv(data, headers):
-        with open(tsv_file, 'w', newline='') as f:
+        with open("output.tsv", 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
             writer.writerow(headers)  # Write the headers
             for key, value in data.items():
                 writer.writerow([key, value])  # Write each key-value pair
 
     # Function to write tabular file from list of list ezomero output
-    def write_table_to_tsv(data):
-        with open(tsv_file, 'w') as f:
+    def write_table_to_tsv(data, id):
+        with open(f"./output/ID_{id}_table.tsv", 'w') as f:
             for row in data:
                 f.write('\t'.join([str(val) for val in row]) + '\n')
 
@@ -35,8 +37,8 @@ def get_object_ezo(user, pws, host, port, obj_type, ids, tsv_file):
             for maid in ids:
                 current_ma_dict = ez.get_map_annotation(conn, maid)
                 ma_dict = {**ma_dict, **current_ma_dict}
+                print(ma_dict)
             write_dict_to_tsv(ma_dict, ["Annotation ID", "Annotation Value"])
-            return ma_dict
         elif obj_type == "Tag":
             tags = []
             for tag_id in ids:
@@ -44,14 +46,18 @@ def get_object_ezo(user, pws, host, port, obj_type, ids, tsv_file):
             # Sort the tags for consistency:
             tags.sort
             write_values_to_tsv(tags, "Tags")
-            return tags
         elif obj_type == "Table":
-            if len(ids) > 1:
-                raise ValueError("Only one table can be exported at a time")
-            table = ez.get_table(conn, ids[0])
-            write_table_to_tsv(table)
-            return table
-
+            for id in ids:
+                table = ez.get_table(conn, id)
+                print(table)
+                write_table_to_tsv(table, id)
+        elif obj_type == ("Attachment"):
+            for id in ids:
+                attch_path = ez.get_file_annotation(conn, id, folder_path='./output/')
+                base_name = os.path.basename(attch_path)
+                df = pd.read_csv(attch_path, sep='\t')
+                df.to_csv(f"./output/ID_{id}_{base_name}", sep='\t', index=False)
+                os.remove(attch_path)
         else:
             sys.exit(f"Unsupported object type: {filter}")
 
@@ -72,8 +78,8 @@ if __name__ == "__main__":
                        help="IDs of the OMERO objects.")
     group.add_argument('--ids_path',
                        help="File with IDs of the OMERO objects (one per line).")
-    parser.add_argument('--tsv_file', default='id_list.tsv', required=True,
-                        help="Output TSV file path.")
+    parser.add_argument('--out_dir', required=True,
+                        help="Output path.")
     args = parser.parse_args()
 
     if args.ids_path:
@@ -95,4 +101,4 @@ if __name__ == "__main__":
                    port=args.port,
                    obj_type=args.obj_type,
                    ids=args.ids,
-                   tsv_file=args.tsv_file)
+                   out_dir=args.out_dir)

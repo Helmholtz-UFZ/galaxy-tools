ac90a92d4f2b6b2c5e9e3bcfea300fc419ea22ce
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2026-01-15 20:23:40 +0100

    Feat: Update the OMERO suite with new credential system (variables/secrets) (#136)
    * feat: include the credential system in the .xml
    
    * feat: updated the python scripts with env variable import and function descriptions
    
    * feat: updated the README file
    
    * fix: Improve credentials section
    
    Co-authored-by: M Bernt <m.bernt@ufz.de>
    
    * feature: omero connection imported as a module
    
    * fix: small fixes and upgrade profile to 25.1
    
    * linting: flake8 for the python scripts
    
    * linting: fixing import groups
    
    * linting: fixing import groups again
    
    * linting: fixing import groups newlines
    
    * fix: converted the credential input into a macro
    
    * fix: correct the macro for credentials input
    
    * fix: add test username and password for testing
    
    * Update GALAXY_BRANCH to release_25.1
    
    * Decrased the tools profile to 25.0
    
    * fix: version bump and corrected the conditional in the xml
    
    * fix: corrected spacing in the xml files
    
    * fix: add missing positional argurment in omero_get_id.py
    
    * fix: correct the arguments order in omero_roi_import.py
    
    * feature: update the security disclaimer
    
    * fixes: applied review comments, 1) add macro for session id option 2) modified credentials macro 3) added required files as xml element
    
    * fix: correct Python import sequence
    
    ---------
    
    Co-authored-by: M Bernt <m.bernt@ufz.de>

---
+++ tools/omero/connect_omero.py
@@ -0,0 +1,15 @@
+import sys
+
+import ezomero as ez
+from omero.gateway import BlitzGateway
+
+
+def establish_connection(uuid_key, usr, psw, host, port):
+    if uuid_key is not None:
+        conn = BlitzGateway(username="", passwd="", host=host, port=port, secure=True)
+        conn.connect(sUuid=uuid_key)
+    else:
+        conn = ez.connect(usr, psw, "", host, port, secure=True)
+    if not conn.connect():
+        sys.exit("ERROR: Failed to connect to OMERO server")
+    return conn
--- tools/omero/omero_metadata_upload.py
+++ tools/omero/omero_metadata_upload.py
@@ -1,13 +1,66 @@
 import argparse
-import json
+import os
 from datetime import datetime
+from pathlib import Path
+from typing import Optional
 
 import ezomero as ez
 import pandas as pd
+from connect_omero import establish_connection
+
+# Import environmental variables
+usr = os.getenv("OMERO_USER")
+psw = os.getenv("OMERO_PASSWORD")
+uuid = os.getenv("UUID_SESSION_KEY")
 
 
-def metadata_import_ezo(user, pws, host, port, obj_type, did=None, ann_type="table", ann_file=None, an_name=None,
-                        log_file='metadata_import_log.txt'):
+def metadata_import_ezo(
+        host: str,
+        port: int,
+        obj_type: str,
+        ann_type: [str] = "table",
+        ann_file: Path = None,
+        an_name: [str] = None,
+        did: Optional[int] = None,
+        uuid_key: Optional[str] = None,
+        log_file: [str] = 'metadata_import_log.txt',
+        ses_close: Optional[bool] = True,
+) -> str:
+
+    '''
+    Import metadata into OMERO as form of OMERO.table or Key-Value Pairs.
+
+    Parameters
+    ----------
+    host : str
+        OMERO server host (i.e. OMERO address or domain name)"
+    port : int
+        OMERO server port (default:4064)
+    did: list
+        ID of the object (if it exists)
+    obj_type : str
+        Annotation type meaning Table or Key-Value pairs
+    ann_type: str
+        Path to the annotation file
+    ann_file: [Path]=None
+        Path to the annotation file
+    an_name : str
+        Namespace or title for the annotation
+    uuid_key : str, optional
+        OMERO UUID session key to connect without password
+    log_file : str
+        Output path for the log file
+    ses_close : bool
+        Decide if close or not the section after executing the script. Defaulf value is true, useful when connecting with the UUID session key.
+
+    Returns
+    -------
+    csv.writer
+        A CSV writer object configured to write TSV data.
+    '''
+
+    conn = establish_connection(uuid_key, usr, psw, host, port)
+
     def upload_metadata(conn, obj_type, did, data_dict, df, ann_type, an_name):
         try:
             if ann_type == "KV":
@@ -48,59 +101,60 @@ def metadata_import_ezo(user, pws, host, port, obj_type, did=None, ann_type="tab
         data_dict = ann_file
 
     try:
-        with ez.connect(user, pws, "", host, port, secure=True) as conn:
-            if obj_type == "project":
-                if did is None:
-                    did = ez.post_project(conn, project_name=str(datetime.now()))
-                result = upload_metadata(conn, "Project", did, data_dict, df, ann_type, an_name)
-            elif obj_type == "screen":
-                if did is None:
-                    did = ez.post_screen(conn, screen_name=str(datetime.now()))
-                result = upload_metadata(conn, "Screen", did, data_dict, df, ann_type, an_name)
-            elif obj_type == "dataset":
-                if did is None:
-                    did = ez.post_dataset(conn, dataset_name=str(datetime.now()))
-                result = upload_metadata(conn, "Dataset", did, data_dict, df, ann_type, an_name)
-            elif obj_type == "plate":
-                result = upload_metadata(conn, "Plate", did, data_dict, df, ann_type, an_name)
-            elif obj_type == "well":
-                result = upload_metadata(conn, "Well", did, data_dict, df, ann_type, an_name)
-            elif obj_type == "image":
-                result = upload_metadata(conn, "Image", did, data_dict, df, ann_type, an_name)
-            else:
-                raise ValueError("Unsupported object type provided: {}".format(obj_type))
-
-            if result is not None:
-                log_success(f"Successfully uploaded metadata for {obj_type} with ID {did}. Result: {result}")
-            else:
-                log_error(f"Failed to upload metadata for {obj_type} with ID {did}.")
-
-        conn.close()
-
-    except Exception as e:
-        log_error(f"Connection error: {str(e)}")
+        if obj_type == "project":
+            if did is None:
+                did = ez.post_project(conn, project_name=str(datetime.now()))
+            result = upload_metadata(conn, "Project", did, data_dict, df, ann_type, an_name)
+        elif obj_type == "screen":
+            if did is None:
+                did = ez.post_screen(conn, screen_name=str(datetime.now()))
+            result = upload_metadata(conn, "Screen", did, data_dict, df, ann_type, an_name)
+        elif obj_type == "dataset":
+            if did is None:
+                did = ez.post_dataset(conn, dataset_name=str(datetime.now()))
+            result = upload_metadata(conn, "Dataset", did, data_dict, df, ann_type, an_name)
+        elif obj_type == "plate":
+            result = upload_metadata(conn, "Plate", did, data_dict, df, ann_type, an_name)
+        elif obj_type == "well":
+            result = upload_metadata(conn, "Well", did, data_dict, df, ann_type, an_name)
+        elif obj_type == "image":
+            result = upload_metadata(conn, "Image", did, data_dict, df, ann_type, an_name)
+        else:
+            raise ValueError("Unsupported object type provided: {}".format(obj_type))
+    finally:
+        if result is not None:
+            log_success(f"Successfully uploaded metadata for {obj_type} with ID {did}. Result: {result}")
+            if ses_close:
+                conn.close()
+        else:
+            log_error(f"Failed to upload metadata for {obj_type} with ID {did}.")
+            if ses_close:
+                conn.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Import metadata into OMERO.')
-    parser.add_argument("--credential-file", dest="credential_file", type=str, required=True,
-                        help="Credential file (JSON file with username and password for OMERO)")
-    parser.add_argument('--host', required=True, help='OMERO host')
-    parser.add_argument('--port', required=True, type=int, help='OMERO port')
-    parser.add_argument('--obj_type', required=True, choices=['project', 'screen', 'dataset', 'plate',
-                                                              'well ', 'image'],
+    parser = argparse.ArgumentParser(
+        description='Import metadata into OMERO as form of OMERO.table or Key-Value Pairs.')
+    parser.add_argument('--host', required=True, help="OMERO server host (i.e. OMERO address or domain name)")
+    parser.add_argument('--port', required=True, type=int, help="OMERO server port (default:4064)")
+    parser.add_argument('--obj_type', required=True,
+                        choices=['project', 'screen', 'dataset', 'plate', 'well ', 'image'],
                         help='Type of OMERO object')
     parser.add_argument('--did', type=int, help='ID of the object (if it exists)')
     parser.add_argument('--ann_type', required=True, choices=['table', 'KV', "attachement"], help='Annotation type')
     parser.add_argument('--ann_file', required=True, help='Path to the annotation file')
     parser.add_argument('--an_name', required=True, help='Namespace or title for the annotation')
+    parser.add_argument('--session_close', required=False, help='Namespace or title for the annotation')
     parser.add_argument('--log_file', default='metadata_import_log.txt', help='Path to the log file')
 
     args = parser.parse_args()
 
-    with open(args.credential_file, 'r') as f:
-        crds = json.load(f)
-
-    metadata_import_ezo(user=crds['username'], pws=crds['password'], host=args.host, port=args.port,
-                        obj_type=args.obj_type, did=args.did, ann_type=args.ann_type,
-                        ann_file=args.ann_file, an_name=args.an_name, log_file=args.log_file)
+    metadata_import_ezo(host=args.host,
+                        port=args.port,
+                        obj_type=args.obj_type,
+                        did=args.did,
+                        ann_type=args.ann_type,
+                        ann_file=args.ann_file,
+                        an_name=args.an_name,
+                        ses_close=args.session_close,
+                        log_file=args.log_file)
